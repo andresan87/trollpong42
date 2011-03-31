1e98a836eec4ddffb5f773de4ee373c506d6d998
unknown <duke@.(none)>
2011-03-31 09:28:26 -0400

    commit inicial

---
+++ pong42.cpp
@@ -0,0 +1,89 @@
+#include "pong42.h" 
+#include <sstream>
+
+using namespace gs2d;
+using namespace gs2d::math;
+
+gs2d::BaseApplicationPtr gs2d::CreateBaseApplication()
+{
+	return BaseApplicationPtr(new Pong);
+}
+
+void Pong::Start(VideoPtr video, InputPtr input, AudioPtr audio)
+{
+	m_video = video;
+	m_input = input;
+	m_barSprite = m_video->CreateSprite(GS_L("assets/textures/pong-bar.png"));
+	m_barSprite->SetOrigin(Vector2(0.5f, 0.5f));
+		
+	for (int x = 0; x < 1; x++) {
+		str_type::stringstream ss;
+		ss << GS_L("assets/textures/") << x << GS_L(".png");
+		SpritePtr sprite = m_video->CreateSprite(ss.str());
+		sprite->SetOrigin(Vector2(0.5f, 0.5f));
+		m_ballSprites.push_back(sprite);
+	}
+	
+	m_ball = boost::shared_ptr<ball>(new ball(m_ballSprites[math::Randomizer::Int(m_ballSprites.size()-1)]));
+	m_bar[0] = boost::shared_ptr<bar>(new bar(m_barSprite));
+	m_bar[1] = boost::shared_ptr<bar>(new bar(m_barSprite));
+
+}
+
+Application::APP_STATUS Pong::Update(unsigned long lastFrameDeltaTimeMS)
+{
+	const Vector2 screenSize = m_video->GetScreenSizeF();
+	const float padding = 20;
+	const float initialVel = 5;
+	if (m_ball->pos == Vector2(-1, -1)) {		
+		m_ball->reset(screenSize, initialVel, m_ballSprites);
+		
+		m_bar[0]->pos = Vector2(padding, screenSize.y/2);
+		m_bar[1]->pos = Vector2(screenSize.x - padding, screenSize.y/2);
+	}
+
+	Vector2 touch;
+	for (int t = 0; t <= 1; t++) {
+		int player = -1;
+		if ((touch = m_input->GetTouchPos(t, m_video)) != GS_NO_TOUCH) {
+			if (touch.x < screenSize.x / 2) {
+				player = 0;
+			}
+			else {
+				player = 1;
+			}
+		}
+		if (player != -1) {
+			m_bar[player]->pos.y += m_input->GetTouchMove(t).y;
+			if (m_bar[player]->pos.y <= 0) {
+				m_bar[player]->pos.y = 0;
+			}
+			else if (m_bar[player]->pos.y >= screenSize.y) {
+				m_bar[player]->pos.y = screenSize.y;
+			}
+		}
+	}
+	m_ball->angle += 1.0f;
+	m_ball->pos += math::Normalize(m_ball->dir) * m_ball->vel;
+	m_ball->wallCollide(screenSize);
+
+	for (int t = 0; t <= 1; t++) {
+		m_bar[t]->ballCollide(m_ball, screenSize, t, m_ballSprites);
+	}
+
+
+	return Application::APP_OK;
+}
+
+void Pong::RenderFrame()
+{
+	m_video->BeginSpriteScene(0xFFEAEAEA);
+	m_barSprite->Draw(m_bar[0]->pos);
+	m_barSprite->Draw(m_bar[1]->pos);
+	m_ball->draw();
+	m_video->EndSpriteScene();
+}
+
+void Pong::Destroy()
+{
+}
---
+++ pong42.h
@@ -0,0 +1,89 @@
+#include "../gs2d/src/gs2dframework.h"
+#include <vector>
+
+namespace gs2d {
+class Pong : public BaseApplication
+{
+	VideoPtr m_video;
+	InputPtr m_input;
+	SpritePtr m_barSprite;
+	std::vector<SpritePtr> m_ballSprites;
+
+	struct ball {
+		math::Vector2 pos;
+		float angle;
+		float vel;
+		math::Vector2 dir;
+		SpritePtr sprite;
+		float ballRadius;
+		
+		ball(SpritePtr spriteBall) {
+			pos = math::Vector2(-1, -1);
+			angle = 0;
+			dir = math::Vector2(0, 0);
+			vel = 0;
+			sprite = spriteBall;
+			ballRadius = sprite->GetBitmapSizeF().x / 2;
+		}
+
+		void wallCollide(const math::Vector2 &screenSize) {
+			if (pos.y >= screenSize.y - ballRadius || pos.y <= ballRadius) {
+				dir.y *= -1;				
+			}
+		}
+
+		void reset(const math::Vector2 &screenSize, const float &initialVel, std::vector<SpritePtr> &m_ballSprites) {
+			dir = math::Vector2(1 - math::Randomizer::Float(2), 0);
+			pos = screenSize / 2;
+			vel = initialVel;
+			sprite = m_ballSprites[math::Randomizer::Int(m_ballSprites.size()-1)];
+		}
+
+		void draw() {
+			sprite->Draw(pos, GS_WHITE, angle);
+		}
+	};
+
+	struct bar {
+		math::Vector2 pos;
+		SpritePtr sprite;
+		math::Vector2 size;
+
+		bar(SpritePtr spriteBar) {
+			pos = math::Vector2(-1, -1);
+			size = spriteBar->GetBitmapSizeF();
+		}
+
+		void ballCollide(boost::shared_ptr<ball> m_ball, const math::Vector2 &screenSize, int bar, std::vector<SpritePtr> &ballSprites) {
+			float leftBall = m_ball->pos.x - m_ball->ballRadius;
+			float rightBall = m_ball->pos.x + m_ball->ballRadius;
+			float bottomBall = m_ball->pos.y + m_ball->ballRadius;
+			float topBall = m_ball->pos.y - m_ball->ballRadius;
+
+			float topBar = pos.y - size.y/2;
+			float bottomBar = pos.y + size.y/2;
+	
+			if ((bar == 1 && rightBall >= pos.x && m_ball->pos.x >= screenSize.x/2) ||
+				(bar == 0 && leftBall <= pos.x && m_ball->pos.x < screenSize.x/2)) {
+				if ((topBall >= topBar && topBall <= topBar) || (bottomBall >= topBar && bottomBall <= bottomBar)) {				
+					m_ball->dir.x *= -1;
+					m_ball->vel *= 1.1f;
+				}
+				else {
+					m_ball->reset(screenSize, 5, ballSprites);
+				}
+			}
+		}
+		
+	};
+	
+	boost::shared_ptr<ball> m_ball;
+	boost::shared_ptr<bar> m_bar[2];
+
+	public:
+		void Start(VideoPtr video, InputPtr input, AudioPtr audio);
+		Application::APP_STATUS Update(unsigned long lastFrameDeltaTimeMS);
+		void RenderFrame();
+		void Destroy();
+};
+} //namespace gs2d
